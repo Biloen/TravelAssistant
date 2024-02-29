0c4ff3b73d238ce70f40b565a2d3f43bbcb760b2
biloen <biloen@DESKTOP-17OR22R>
2024-03-01 00:04:36 +0100

    Add project files.

---
+++ TravelAssistant/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelAssistant
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
